c12a10f99075870c317773745de05ec3219f86fb
Ravikumar <ravikumark1150@gmail.com>
2025-01-27 23:51:33 +0530

    customer project 27-01

---
+++ src/main/java/com/customer/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.customer.controller;
+
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";  // This corresponds to /WEB-INF/views/home.jsp
+    }
+}
+
---
+++ src/main/java/com/customer/entity/Customer.java
@@ -0,0 +1,28 @@
+package com.customer.entity;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Customer {
+	
+	
+	public static void main(String args[]) {
+		
+		
+
+		String str = "ravikumar";
+		
+		  Set<Character> collect = str.chars().mapToObj(c->(char)c)
+		              .collect(Collectors.groupingBy(c->c,Collectors.counting()))
+		              .entrySet()
+		              .stream()
+		              .filter(dublicate-> dublicate.getValue()>1)
+		              .map(Map.Entry :: getKey)
+		              .collect(Collectors.toSet());
+		
+		  
+		  System.out.println("The Dublicate Character IS : "+collect);
+		
+	}
+}
