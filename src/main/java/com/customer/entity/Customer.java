bc533d4a7fc85052bdffa8a36f6d524b4212188c
Ravikumar <ravikumark1150@gmail.com>
2025-01-28 22:08:54 +0530

    dao modification

--- src/main/java/com/customer/entity/Customer.java
+++ src/main/java/com/customer/entity/Customer.java
@@ -13,16 +13,26 @@ public class Customer {
 
 		String str = "ravikumar";
 		
-		  Set<Character> collect = str.chars().mapToObj(c->(char)c)
-		              .collect(Collectors.groupingBy(c->c,Collectors.counting()))
-		              .entrySet()
-		              .stream()
-		              .filter(dublicate-> dublicate.getValue()>1)
-		              .map(Map.Entry :: getKey)
-		              .collect(Collectors.toSet());
+		/*
+		 * Set<Character> collect = str.chars().mapToObj(c->(char)c)
+		 * .collect(Collectors.groupingBy(c->c,Collectors.counting())) .entrySet()
+		 * .stream() .filter(dublicate-> dublicate.getValue()>1) .map(Map.Entry ::
+		 * getKey) .collect(Collectors.toSet());
+		 * 
+		 */
+		Set<Character> collect = str.chars().mapToObj(c->(char)c)
+		            .collect(Collectors.groupingBy(c->c,Collectors.counting()))
+		            .entrySet()
+		            .stream()
+		            .filter(d-> d.getValue()>1)
+		            .map(Map.Entry :: getKey)
+		            .collect(Collectors.toSet());
+		            
 		
-		  
 		  System.out.println("The Dublicate Character IS : "+collect);
+		  
+		  
+		  
 		
 	}
 }
